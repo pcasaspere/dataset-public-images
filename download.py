6d0d20369fddcc43ef0b0f9996c41d61668d0975
Pere Casas <pcasas.pere@gmail.com>
2023-07-18 17:41:59 +0200

    add python script

---
+++ download.py
@@ -0,0 +1,39 @@
+import os
+import datetime
+import requests
+
+
+def downloadAndSaveImage(url: str, save_path: str) -> None:
+    try:
+        response = requests.get(url)
+        response.raise_for_status()
+        with open(save_path, "wb") as f:
+            f.write(response.content)
+    except requests.exceptions.RequestException as e:
+        print(f"Error en descargar l'imatge: {e}")
+
+
+if __name__ == "__main__":
+    print('Start script')
+
+    images_directory = "images"
+
+    targets = {
+        'vic': 'http://www.meteosona.com/static/estacions/images/webcams/VicPlaca.jpg'
+    }
+
+    if not os.path.exists(images_directory):
+        os.makedirs(images_directory)
+
+    for key in targets:
+        target = targets[key]
+        extension = target.split('.')[-1]
+        images_directory_target = f"{images_directory}/{key}"
+        file_name = datetime.datetime.now().strftime("%Y_%m_%d_%H_%M") + "." + extension
+
+        if not os.path.exists(f"{images_directory_target}"):
+            os.makedirs(images_directory_target)
+
+        save_path = os.path.join(f"{images_directory_target}", file_name)
+
+        downloadAndSaveImage(targets[key], save_path)
